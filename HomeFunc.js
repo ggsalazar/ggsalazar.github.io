b17084dc5a04f92546517a17a00df7fb5d72e96d
ggsalazar <87739359+ggsalazar@users.noreply.github.com>
2023-04-29 23:27:45 -0500

    Added all files

---
+++ HomeFunc.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.CharSheetsbtn')) {
+    var dropdowns = document.getElementsByClassName("CharSheets-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
---
+++ Pre-Made Content/PreFunc.js
@@ -0,0 +1,7 @@
+function openSide() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeSide() {
+  document.getElementById("mySidenav").style.width = "0";
+}
