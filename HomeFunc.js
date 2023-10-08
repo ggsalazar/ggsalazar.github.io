51f84bc57640dcc1c13c665b0bbef605dabbf832
ggsalazar <87739359+ggsalazar@users.noreply.github.com>
2023-10-08 01:56:23 -0500

    Changed the whole website

--- HomeFunc.js
+++ HomeFunc.js
@@ -1,17 +1,29 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+//Slideshow stuff
+var slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.CharSheetsbtn')) {
-    var dropdowns = document.getElementsByClassName("CharSheets-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
+
+function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+} 
--- Pre-Made Content/PreFunc.js
+++
@@ -1,7 +0,0 @@
-function openSide() {
-    document.getElementById("mySidenav").style.width = "300px";
-}
-
-function closeSide() {
-  document.getElementById("mySidenav").style.width = "0";
-}
